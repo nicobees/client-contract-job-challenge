86344a7611030c3651ba60163e14800c3b1e835c
Nicola Abis <n.abis@e-motion.com>
2021-12-24 14:37:23 +0100

    API#3 GET jobs unpaid

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ app.use(bodyParser.json());
 app.set('sequelize', sequelize)
 app.set('models', sequelize.models)
 
+/* @Component: Contract */
 /**
  * Get Contract by Id
  * 
@@ -21,7 +22,7 @@ app.get('/contracts/:id', getProfile ,async (req, res) =>{
     const {Contract} = req.app.get('models')
     const {id} = req.params
 
-    /* Repository */
+    /* @Repository */
     const contract = await Contract.findOne(
         {
             where: {
@@ -34,7 +35,7 @@ app.get('/contracts/:id', getProfile ,async (req, res) =>{
         }
     )
     /**************/
-    
+
     if(!contract) return res.status(404).end()
     res.json(contract)
 })
@@ -47,7 +48,7 @@ app.get('/contracts/:id', getProfile ,async (req, res) =>{
  app.get('/contracts/', getProfile ,async (req, res) =>{
     const {Contract} = req.app.get('models')
     
-    /* Repository */
+    /* @Repository */
     const contracts = await Contract.findAll(
         {
             where: {
@@ -63,6 +64,45 @@ app.get('/contracts/:id', getProfile ,async (req, res) =>{
     )
     /**************/
     res.json(contracts)
+})
+
+/* @Component: Job */
+/**
+ * Get all unpaid Jobs for only the "active" contracts of the logged Profile
+ * 
+ * @returns jobs list
+ */
+ app.get('/jobs/unpaid', getProfile, async (req, res) =>{
+    const {Contract, Job} = req.app.get('models')
+    
+    /* Repository */
+    const jobs = await Job.findAll(
+        {
+            where: {
+                [Op.and]: [
+                    {[Op.or]: [
+                        { paid: false },
+                        { paid: { [Op.is]: null }}
+                    ]},
+                    {'$Contract.status$': {
+                        [Op.notIn]: ['terminated'],
+                    }},
+                    {[Op.or]: [
+                        {'$Contract.ClientId$': req.profile.id},
+                        {'$Contract.ContractorId$': req.profile.id}
+                    ]}
+                ]
+            },
+            include: {
+                model: Contract,
+                as: 'Contract',
+                required: true,
+                attributes: []
+            }
+        }
+    )
+    /**************/
+    res.json(jobs)
 })
 
 module.exports = app;
