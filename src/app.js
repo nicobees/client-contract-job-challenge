f7a5585e3ce18fc27fc805d2be3793bcbd8356e5
Nicola Abis <n.abis@e-motion.com>
2021-12-24 11:58:45 +0100

    API#1: now returning only contracts "owned" by the logged Profile
    - both the Client and the Contractor can GET the contract
    - open point: in case of request from a "not owner" profile, should
    403-forbidden be returned? This depends on the feature specs

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { Op } = require("sequelize");
+
 const {sequelize} = require('./model')
 const {getProfile} = require('./middleware/getProfile')
 const app = express();
@@ -14,7 +16,17 @@ app.set('models', sequelize.models)
 app.get('/contracts/:id',getProfile ,async (req, res) =>{
     const {Contract} = req.app.get('models')
     const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
+    const contract = await Contract.findOne(
+        {
+            where: {
+                id: id,
+                [Op.or]: [
+                    {ClientId: req.profile.id},
+                    {ContractorId: req.profile.id}
+                ]
+            }
+        }
+    )
     if(!contract) return res.status(404).end()
     res.json(contract)
 })
