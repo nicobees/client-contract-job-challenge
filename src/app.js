582fd7a4225461cd24e3efe0bbc94900715e6cf9
Nicola Abis <n.abis@e-motion.com>
2021-12-24 23:33:56 +0100

    API#7: GET most paying Clients in date range

--- src/app.js
+++ src/app.js
@@ -265,7 +265,7 @@ app.get('/contracts/:id', getProfile ,async (req, res) =>{
 
 /* @Component: Admin */
 /**
- * Return best profession in a specific time range
+ * Return best profession in a specific date range
  * 
  * @GET
  * @returns profession
@@ -313,11 +313,81 @@ app.get('/contracts/:id', getProfile ,async (req, res) =>{
     )
     /**************/
 
-    const bestProfession = jobsProfessions.reduce(function(prev, current) {
+    const bestProfession = jobsProfessions.reduce((prev, current) => {
         return (prev.sumPrice > current.sumPrice) ? prev : current
     })
 
     res.json({result: true, data: {profession: bestProfession.profession}})
+})
+
+/* @Component: Admin */
+/**
+ * Return the list of Clients (Profile) that most paid for Jobs in a specific date range
+ * 
+ * @GET
+ * @returns Profile clients list
+ */
+ app.get('/admin/best-clients', getProfile, async (req, res) =>{
+    const {Contract, Job, Profile} = req.app.get('models')
+    const {start, end, limit} = req.query
+
+    const startDate = new Date(start)
+    const endDate = new Date(`${end} 23:59:59.999 GMT`)
+
+    const limitValue = limit || 2
+
+    /* @Repository */
+    const clientTotalPayments = await Job.findAll(
+        {
+            attributes: [
+                'Contract.Client.id',
+                'Contract.Client.firstName',
+                'Contract.Client.lastName',
+                // [sequelize.col('Contract.Client.fullName'), 'fullName'],
+                [sequelize.fn('sum', sequelize.col('price')), 'paid']
+            ],
+            raw: true,
+            where: {
+                [Op.and]: [
+                    { paid: true },
+                    {
+                        paymentDate: {
+                            [Op.gte]: startDate,
+                            [Op.lte]: endDate
+                        }
+                    }
+                ]
+            },
+            include: {
+                model: Contract,
+                as: 'Contract',
+                required: true,
+                attributes: [],
+                include: {
+                    model: Profile,
+                    as: 'Client',
+                    required: true,
+                    attributes: []
+                }
+            },
+            group: ['Contract.Client.id'],
+            order: [
+                [sequelize.col('paid'), 'DESC']
+            ],
+            limit: limitValue
+        }
+    )
+    /**************/
+    
+    const result = clientTotalPayments.map(client => {
+        return {
+            id: client.id,
+            fullName: `${client.firstName} ${client.lastName}`,
+            paid: client.paid
+        }
+    })
+
+    res.json(result)
 })
 
 module.exports = app;
