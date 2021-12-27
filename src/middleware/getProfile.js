453491e516d491e94267e16b2070a6d76ff9cad0
Nicola Abis <n.abis@e-motion.com>
2021-12-27 01:14:19 +0100

    Updated Profile middleware injected inside function

--- src/middleware/getProfile.js
+++ src/middleware/getProfile.js
@@ -1,9 +1,10 @@
 
-const getProfile = async (req, res, next) => {
-    const {Profile} = req.app.get('models')
-    const profile = await Profile.findOne({where: {id: req.get('profile_id') || 0}})
-    if(!profile) return res.status(401).end()
-    req.profile = profile
-    next()
+const getProfile = (profileRepository) => {
+    return async (req, res, next) => {
+        const profile = await profileRepository.getProfileById(req.get('profile_id'))
+        if(!profile) return res.status(401).end()
+        req.profile = profile
+        next()
+    }
 }
 module.exports = {getProfile}
