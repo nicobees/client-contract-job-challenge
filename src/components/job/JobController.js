351212d53a6faac0fac94b4fea4257b44c05b4d6
Nicola Abis <n.abis@e-motion.com>
2021-12-27 01:20:40 +0100

    Job component

---
+++ src/components/job/JobController.js
@@ -0,0 +1,62 @@
+const { Router } = require("express")
+const { Controller } = require("../../classes/Controller.class")
+const { NotFoundError, ClientCanNotPay, JobPayError } = require("../../common/exceptions")
+
+class JobController extends Controller {
+    constructor(middlewares, jobService) {
+        super()
+        this.setContext('Jobs')
+        this.setPath('/jobs')
+
+        this.setRouter(Router())
+        this.jobService = jobService
+
+        this.getRouter().get(`${this.getPath()}/unpaid`, middlewares.getProfile, this.getUnpaidJobs)
+        this.getRouter().post(`${this.getPath()}/:job_id/pay`, middlewares.getProfile, this.payJob)
+    }
+
+    /**
+     * Get all unpaid Jobs for only the "active" contracts of the logged Profile
+     * 
+     * @GET
+     * @returns jobs list
+     */
+     getUnpaidJobs = async (req, res, next) => {
+        const jobs = await this.jobService.getUnpaidJobsInActiveContracts(req.profile.id)
+
+        res.json(jobs)
+    }
+
+    /**
+     * Pay for the specified Job: the Job price will be moved from the Client balance to the Contractor balance
+     * 
+     * @POST
+     * @returns job updated
+     */
+    payJob = async (req, res, next) => {
+        try {
+            const {job_id} = req.params
+            
+            const paidJob = await this.jobService.payJob(job_id, req.profile)
+    
+            res.json(paidJob)
+        } catch(e) {
+            if(e instanceof NotFoundError) {
+                res.status(404).end()
+                return
+            }
+            if(e instanceof ClientCanNotPay) {
+                res.status(400).json({result: false, message: e.message}).end()
+                return
+            }
+            if(e instanceof JobPayError) {
+                res.status(500).json({result: false, message: e.message}).end()
+            }
+            throw e
+        }
+    }
+}
+
+module.exports = {
+    JobController
+}
---
+++ src/components/job/JobService.js
@@ -0,0 +1,43 @@
+const { Service } = require("../../classes/Service.class")
+const { NotFoundError, ClientCanNotPay } = require("../../common/exceptions")
+
+class JobService extends Service {
+    jobRepository
+
+    constructor(jobRepository) {
+        super()
+        this.setContext('Jobs')
+        this.jobRepository = jobRepository
+    }
+
+    getUnpaidJobsInActiveContracts = async (ownerId) => {
+        return await this.jobRepository.getUnpaidJobs(ownerId, ['terminated'], true)
+    }
+    
+    payJob = async (jobId, client) => {
+        // 1) Get Job data: only if the Job is unpaid and only if logged Profile is Client of the Contract of the specified Job, otherwise throw error in order to return 404 (open point)
+        const job = await this.jobRepository.getUnpaidClientJobById(jobId, client.id, ['terminated'], true)
+
+        if(!job) {
+            throw new NotFoundError(`Job with id ${jobId} not found`)
+        }
+
+        // 2) check if logged Profile (being a Client) has enough Balance to pay for the specified Job: if no, response is 400 with custom message error
+        if(!client.balance || client.balance < job.price) {
+            throw new ClientCanNotPay(`Current logged user has not enough Balance to pay for Job with id ${jobId}`)
+        }
+
+        // 3) if we reach this point, then the Job can be paid:
+        // 3.1) then move the balance from the Client to the Contractor
+        // 3.2) then update the Job to reflect the paiment
+        // IMPORTANT: "transaction" will be used from Sequelize in order to manage the two operations 3.1 and 3.2 consistently in case of errors
+
+        const fullJobUpdated = await this.jobRepository.payJob(job)
+
+        return fullJobUpdated
+    }
+}
+
+module.exports = {
+    JobService
+}
