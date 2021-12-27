d56bbdb4118e8e44d130a083cc7c850161408ece
Nicola Abis <n.abis@e-motion.com>
2021-12-27 20:35:12 +0100

    Profile component with balances API
    - also getProfile middleware has been aligned accordingly (now
    ProfileService is injected into getProfile middleware instead of
    ProfileRepository)

--- src/components/profile/ProfileRepository.js
+++ src/components/profile/ProfileRepository.js
@@ -1,18 +1,59 @@
+const { Op } = require("sequelize")
+
 const { Repository } = require("../../classes/Repository.class")
 
 class ProfileRepository extends Repository {
     constructor(db) {
         super()
-        this.setContext('Profile')
+        this.setContext('Profile/Balance')
         this.models = db.models
+        this.db = db
     }
 
     getProfileById = async (profileId) => {
-        const {Profile} = this.models
+        const { Profile } = this.models
         
-        const profile = await Profile.findOne({where: {id: profileId || 0}})
+        const profile = await Profile.findOne({where: {id: profileId}})
 
         return profile
+    }
+
+    getPriceSumUnpaidJobsByClientId = async (clientId) => {
+        const { Job, Contract } = this.models
+        
+        const maxDepositAmount = await Job.sum('price',
+            {
+                where: {
+                    [Op.and]: [
+                        {[Op.or]: [
+                            { paid: false },
+                            { paid: { [Op.is]: null }}
+                        ]},
+                        {'$Contract.status$': {
+                            [Op.notIn]: ['terminated'],
+                        }},
+                        {[Op.or]: [
+                            {'$Contract.ClientId$': clientId}
+                        ]}
+                    ]
+                },
+                include: {
+                    model: Contract,
+                    as: 'Contract',
+                    required: true,
+                    attributes: []
+                }
+            }
+        )
+
+        return maxDepositAmount
+    }
+
+    depositAmountIntoClientBalance = async (clientId, amount) => {
+        const userProfile = await this.getProfileById(clientId)
+        const updatedProfile = await userProfile.increment({balance: amount})
+
+        return await updatedProfile.reload()
     }
 }
 
